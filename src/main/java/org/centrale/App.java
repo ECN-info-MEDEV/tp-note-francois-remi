84d97c9a8a25841d2a5a3ef61da6b25b0aa22278
Inky19 <f.marie260@gmail.com>
2023-01-26 10:29:36 +0100

    refactored clues and added  guess

--- src/main/java/org/centrale/App.java
+++ src/main/java/org/centrale/App.java
@@ -10,5 +10,14 @@ public class App
     {
         Pawn pawn1 = new Pawn(Pawn.YELLOW);
         Pawn pawn2 = new Pawn(8);
+        Guess guess = new Guess();
+        guess.add(pawn1);
+        System.out.println(guess);
+        Clue aled = new Clue(3);
+        System.out.println(pawn1.maxColor());
+        System.out.println(aled.maxColor());
+        int[] test = new int[4]; 
+        test[0] = 2;
+        System.out.println(test);
     }
 }

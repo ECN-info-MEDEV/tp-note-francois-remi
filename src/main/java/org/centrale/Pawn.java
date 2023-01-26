d34fc421b5bffd83db6c6c7ee77216b521ee95fc
Inky19 <f.marie260@gmail.com>
2023-01-26 09:02:29 +0100

    Added Pawn and Execption for wrong value

--- src/main/java/org/centrale/Pawn.java
+++ src/main/java/org/centrale/Pawn.java
@@ -10,4 +10,39 @@ package org.centrale;
  */
 public class Pawn {
     
+    public static int YELLOW = 0;
+    public static int BLUE = 1;
+    public static int RED = 2;
+    public static int GREEN = 3;
+    public static int WHITE = 4;
+    public static int BLACK = 5;
+    
+    
+    private int color;
+    
+    public Pawn () {
+        color = RED;
+    }
+
+    public Pawn (int color){
+        try{
+            if (color < 0 || color > 5){
+                throw new WrongValueException(color);
+            }
+            this.color = color;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Defaulting to RED.");
+            this.color = RED;
+        }
+    }
+    
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+        
 }
